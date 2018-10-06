37e51fcf493d6fc85285a97e392a1060a1e862f1
IsmarySalgesM <ismarysalges@gmail.com>
2018-10-05 23:03:55 -0300

    funcionalidad flipcard lista

--- src/Carta/Carta.js
+++ src/Carta/Carta.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import Flipcard from "react-flipcard-2"
 import '../Carta/Carta.css';
+
+
 
 class Carta extends Component {
     render() {
         return (
             <div className="carta">
-           <i className={`fa ${this.props.icono} fa-5x`}></i>
+             <Flipcard>
+                    <div className="portada"></div>
+                    <div className="contenido"></div>
+                    <i className={`fa ${this.props.icono} fa-5x`}></i>
+            
+                </Flipcard>
             </div>
         );
     }
