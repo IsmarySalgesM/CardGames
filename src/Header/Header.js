1e4a3cba04f23adce8cf53b2c1d7d1459b9c98eb
IsmarySalgesM <ismarysalges@gmail.com>
2018-10-21 15:29:56 -0300

    mejorando responsive

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <div><h1 class="titulo">l</h1></div>
+                <div class="titulo">Juego de Memoria</div>
             </header>
         );
     }
